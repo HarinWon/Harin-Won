8d373b85f1442f5c4016c777b975dc5b38264317
HarinWon <kidstol1217@naver.com>
2024-07-04 18:20:52 +0900

    “240704 git push origin main git remote add origin https://github.com/HarinWon/Harin-Won.git git pull origin main git add . git status git commit -m “240704

--- homework2/About_Me/script.js
+++ homework2/About_Me/script.js
@@ -1,26 +1,16 @@
-// overlay
 document.addEventListener("DOMContentLoaded", () => {
-  const overlay = document.getElementById("overlay");
-  const watchLinks = document.querySelectorAll(".watch");
-  const closeOverlay = document.getElementById("closeOverlay");
+  const btn = document.querySelector("#open");
+  const modalBox = document.querySelector("#modal_box");
+  const close = document.querySelector("#close");
 
-  watchLinks.forEach((watch) => {
-    watch.addEventListener("click", (event) => {
-      event.preventDefault(); // 링크의 기본 동작을 막습니다
-      overlay.style.display = "flex"; // 오버레이를 표시합니다
-    });
+  btn.addEventListener("click", () => {
+    modalBox.classList.add("active");
   });
 
-  closeOverlay.addEventListener("click", () => {
-    overlay.style.display = "none"; // 오버레이를 숨깁니다
+  close.addEventListener("click", () => {
+    modalBox.classList.remove("active");
   });
 
-  // 오버레이 바깥을 클릭하면 오버레이를 숨깁니다
-  overlay.addEventListener("click", (event) => {
-    if (event.target === overlay) {
-      overlay.style.display = "none";
-    }
+  
   });
-});
 
-// profile
