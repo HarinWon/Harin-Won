aa1495edb2adf8d72d2aba68c64ea9d53ab476a3
HarinWon <kidstol1217@naver.com>
2024-07-24 16:36:36 +0900

    240724

--- 240723/kakaomap/script.js
+++ 240723/kakaomap/script.js
@@ -19,26 +19,36 @@ const showPosition = (position) => {
       title: "올리브영 강남우성점",
       latlng: new kakao.maps.LatLng(37.4918902, 127.0309525),
       adress: "서울시 강남구 강남대로 320",
+      img: "./img/img1.jpg",
+      info: "영업시간 : 오전 8시 ~ 저녁 10시",
     },
     {
       title: "올리브영 강남중앙점",
       latlng: new kakao.maps.LatLng(37.4962484, 127.0287983),
       adress: "서울시 강남구 강남대로 374",
+      img: "./img/img2.jpg",
+      info: "영업시간 : 오전 9시 ~ 저녁 9시",
     },
     {
       title: "올리브영 서초타운점",
       latlng: new kakao.maps.LatLng(37.4950544, 127.0280286),
       adress: "서울시 강남구 서초대로 78길",
+      img: "./img/img3.jpg",
+      info: "영업시간 : 오전 10시 ~ 저녁 9시",
     },
     {
       title: "올리브영 서초대로점",
       latlng: new kakao.maps.LatLng(37.4940977, 127.0158067),
       adress: "서울시 강남구 서초대로 314",
+      img: "./img/img4.jpg",
+      info: "영업시간 : 오전 9시 ~ 저녁 10시",
     },
     {
-      title: "올리브영 역삼점",
-      latlng: new kakao.maps.LatLng(37.4918902, 127.0309525),
+      title: "올리브영 역삼역점",
+      latlng: new kakao.maps.LatLng(37.4996723, 127.0342751),
       adress: "서울시 강남구 테헤란로 111",
+      img: "./img/img5.jpg",
+      info: "영업시간 : 오전 9시 ~ 저녁 8시",
     },
   ];
 
@@ -48,33 +58,28 @@ const showPosition = (position) => {
       position: positions[i].latlng,
     });
 
-    const infowindow = new kakao.maps.InfoWindow({
-      content: positions[i].title,
+    const content = `
+    <div class="wrap">
+      <div class="info">
+        <div class="title">${positions[i].title}</div>
+        <div class="body">
+          <div class="img">
+            <img src="${positions[i].img}" width="73" height="70">
+          </div>
+          <div class="desc">
+            <div class="ellipsis">${positions[i].adress}</div>
+            <div class="jibun ellipsis">${positions[i].info}</div>
+            <div><a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0" target="_blank" class="link">쇼핑몰 바로가기</a></div>
+          </div>
+        </div>
+      </div>
+    </div>;
+    `;
+    const overlay = new kakao.maps.CustomOverlay({
+      content: content,
+      map: map,
+      position: marker.getPosition(),
     });
-
-    const makeOverListner = (map, marker, infowindow) => {
-      return () => {
-        infowindow.open(map, marker);
-      };
-    };
-
-    const makeOutListner = () => {
-      return () => {
-        infowindow.close();
-      };
-    };
-
-    kakao.maps.event.addListener(
-      marker,
-      "mouseover",
-      makeOverListner(map, marker, infowindow)
-    );
-
-    kakao.maps.event.addListener(
-      marker,
-      "mouseout",
-      makeOutListner(infowindow)
-    );
   }
 
   // 현재 위치에 마커 추가
@@ -87,16 +92,13 @@ const showPosition = (position) => {
   marker.setMap(map);
 
   // Kakao Marker InfoWindow
-  const iwContent = '<div class="iwItem">현재 내 위치</div>';
-  const iwRemoveable = true;
-  const infowindow = new kakao.maps.InfoWindow({
+  const iwContent = `<div class ="label"><span class="left"></span><span class="center">🎈현재위치</span><span class="right"></span></div>`;
+  const iwPosition = new kakao.maps.LatLng(latitude, longitude);
+  const infowindow = new kakao.maps.CustomOverlay({
     content: iwContent,
-    removable: iwRemoveable,
+    position: iwPosition,
   });
-  kakao.maps.event.addListener(marker, "click", function () {
-    // 마커 위에 인포윈도우를 표시합니다
-    infowindow.open(map, marker);
-  });
+  infowindow.setMap(map);
 };
 
 // 위치 정보 요청 실패 시 호출되는 함수
