e7121f59c16ea01985022173f0bab692235a98e0
HarinWon <kidstol1217@naver.com>
2024-07-29 18:11:52 +0900

    240729

---
+++ 01JS/script.js
@@ -0,0 +1,44 @@
+const test1 = "mon";
+
+// 프로그램을 개발
+// - width 1920px
+// - 슬라이드 개수 5개 고정
+
+// 스크롤을 내리게 되었을 때, 내려간 값에 따라서 무언가를 변경!!
+
+// let 재선언 x 재할당 o
+let test2 = "mon";
+
+test2 = "tue";
+
+// 조별프로젝트
+// A
+// 카테고리 안에 상품개수 10개
+var category = 8;
+
+// B
+// 장바구니에 담긴 상품개수
+var category = 10;
+
+// 병합
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+
+// console.log(typeof sum);
+
+const str = "1";
+
+const num = 1000;
+const string = "원";
+
+const counter = num + string;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+const counter1 = num1 + str1;
+console.log(counter1);
+
+const input = prompt("얼마를 원하세요?");
