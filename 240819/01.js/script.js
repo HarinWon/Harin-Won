d6717f56b7779c2eeb7826cfa6cfc03f9604193e
HarinWon <kidstol1217@naver.com>
2024-08-21 16:56:42 +0900

    240821

--- 240819/01.js/script.js
+++ 240819/01.js/script.js
@@ -1,48 +1,42 @@
-const playButon = document.querySelector(".play-pause");
+const playButton = document.querySelector(".play-pause");
 const video = document.querySelector("video");
 const volumeBar = document.querySelector("input[type='range']");
-const progressCover = document.querySelector(".prograss");
+const progressCover = document.querySelector(".progress");
 const player = document.querySelector(".player");
+const rateButtons = document.querySelectorAll(".rate");
+const FullButton = document.querySelector("#fullscreenBtn");
 
+console.log(rateButtons);
 const play = () => {
-  playButon.innerText = "| |";
+  playButton.innerText = "| |";
   video.play();
 };
-
 const pause = () => {
-  playButon.innerText = "▶";
+  playButton.innerText = "▶";
   video.pause();
 };
-
 const togglePlay = () => {
   video.paused ? play() : pause();
 };
-
 const setVolume = (e) => {
   video.volume = e.target.value;
 };
-
 const formatting = (time) => {
   let sec = Math.floor(time % 60);
   let min = Math.floor(time / 60);
   let hour = Math.floor(time / 3600);
-
   sec = sec < 10 ? `0${sec}` : sec;
   min = min < 10 ? `0${min}` : min;
   hour = hour < 10 ? `0${hour}` : hour;
-
   return `${hour}:${min}:${sec}`;
 };
-
 const updateTime = () => {
   const current = document.querySelector(".current");
   const duration = document.querySelector(".duration");
-  console.log(current, duration);
-
   current.innerText = formatting(video.currentTime);
   duration.innerText = formatting(video.duration);
 };
-
+//프로그래스 바 시간에 따라 움직이게!
 const updateProgress = () => {
   const progressBar = document.querySelector(".bar");
   const progressPointer = document.querySelector(".circle");
@@ -52,25 +46,39 @@ const updateProgress = () => {
   progressBar.style.width = `${percent}%`;
   const progressBarWidth = progressCover.clientWidth;
   const newPosition = (currentTime / duration) * progressBarWidth - 1;
-  console.log(progressBarWidth);
   progressPointer.style.left = `${newPosition}px`;
+  // console.log(newPosition);
 };
-
 const videoPoint = (e) => {
   console.log(e.pageX);
   console.log(player.offsetLeft);
-  const moustX = e.pageX - player.offsetLeft;
+  const mouseX = e.pageX - player.offsetLeft;
   const progressBarWidth = progressCover.clientWidth;
   const duration = video.duration;
-  const clickedTime = (moustX / progressBarWidth) * duration;
+  const clickedTime = (mouseX / progressBarWidth) * duration;
   video.currentTime = clickedTime;
 };
 
-playButon.addEventListener("click", togglePlay);
+const setRate = (e) => {
+  const { rate } = e.target.dataset;
+  video.playbackRate = rate;
+};
+
+playButton.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateTime);
-video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("timeupdate", updateProgress); //프로그래스 바 시간에 따라 움직이게!
 volumeBar.addEventListener("change", setVolume);
 progressCover.addEventListener("click", (e) => {
   videoPoint(e);
+});
+rateButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    setRate(e);
+  });
+});
+
+// FullScreen
+FullButton.addEventListener("click", () => {
+  video.requestFullscreen();
 });
